346f4fffb1934c87aca57f5b9c8c4ca97662b72f
CKX-dev <devckx@gmail.com>
2024-02-15 20:31:22 +0700

    redeploy1

--- pages/referral.js
+++ pages/referral.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 import { Button } from 'web3uikit'
 import NavBar from '../components/NavBar'
 import { getData, saveData, updateData } from '../utils/localStorage'
-import Referral from '../components/Referral/index.jsx'
+import Referral from '../components/referral'
 import { Context, ContextProvider } from "../context/Context"
 
 export default function referral() {
